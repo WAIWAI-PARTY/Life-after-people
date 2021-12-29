30b31cbf51403d11a169ef29d0ba1105bdf5cd2f
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2021-12-30 00:14:18 +0800

    basic movement basic background

---
+++ Player.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+
+export var maxSpeed = 100
+export var acceleration = 1000
+export var friction = 1000
+
+var velocity = Vector2.ZERO
+
+func _process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector*maxSpeed, acceleration*delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+	
+	move_and_slide(velocity)
+	
+
