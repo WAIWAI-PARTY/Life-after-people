ef6b2c5c222c790062876f78f5d40cdd7fcf3bff
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2021-12-30 18:11:38 +0800

    with flip but fixing central point needed

--- Weapons/Gun.gd
+++ Weapons/Gun.gd
@@ -1,6 +1,19 @@
-extends KinematicBody2D
 
+extends Node2D
 
-func _process(delta):
+func _input(event):
+	if event is InputEventMouseMotion:
+		look_at(event.position)
+
+		# keep rotation_degrees between 0 and 360
+		rotation_degrees = fposmod(rotation_degrees, 360.0)
+
+		if rotation_degrees > 90 && rotation_degrees < 270:
+			$GunSprite.flip_v = true
+			
+		else:
+			$GunSprite.flip_v = false
+			
+			
 	
-	look_at(get_global_mouse_position())
+
