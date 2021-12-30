62fb5c43e60f351837afc62d15126bca03c62a80
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2021-12-30 19:24:32 +0800

    gun fix so ez

--- Weapons/Gun.gd
+++ Weapons/Gun.gd
@@ -7,13 +7,4 @@ func _input(event):
 
 		# keep rotation_degrees between 0 and 360
 		rotation_degrees = fposmod(rotation_degrees, 360.0)
-
-		if rotation_degrees > 90 && rotation_degrees < 270:
-			$GunSprite.flip_v = true
-			
-		else:
-			$GunSprite.flip_v = false
-			
-			
-	
-
+		$GunSprite.flip_v = rotation_degrees > 90 && rotation_degrees < 270
