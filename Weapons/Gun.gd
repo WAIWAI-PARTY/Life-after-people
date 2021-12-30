adc7af6d5414f299d007448e945ef5f26a3ee45f
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2021-12-30 19:34:03 +0800

    Revert "gun fix so ez"
    This reverts commit 62fb5c43e60f351837afc62d15126bca03c62a80.

--- Weapons/Gun.gd
+++ Weapons/Gun.gd
@@ -7,4 +7,13 @@ func _input(event):
 
 		# keep rotation_degrees between 0 and 360
 		rotation_degrees = fposmod(rotation_degrees, 360.0)
-		$GunSprite.flip_v = rotation_degrees > 90 && rotation_degrees < 270
+
+		if rotation_degrees > 90 && rotation_degrees < 270:
+			$GunSprite.flip_v = true
+			
+		else:
+			$GunSprite.flip_v = false
+			
+			
+	
+
