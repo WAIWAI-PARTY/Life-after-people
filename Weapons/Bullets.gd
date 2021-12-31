03e1506e8074e186aaa11c451b775f928119e6ec
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2021-12-31 11:55:12 +0800

    fix position2d now

---
+++ Weapons/Bullets.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+export var speed = 100
+
+func _ready():
+	set_as_toplevel(true)
+
+func _process(delta):
+	position += (Vector2.RIGHT*speed).rotated(rotation) * delta
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+func _on_Bullets_body_entered(body):
+	queue_free()
--- Weapons/Gun.gd
+++ Weapons/Gun.gd
@@ -1,6 +1,10 @@
 
 extends Node2D
 
+var can_fire = true
+var bullet = preload("res://Weapons/Bullets.tscn")
+
+
 func _input(event):
 	if event is InputEventMouseMotion:
 		look_at(event.position)
@@ -8,3 +12,12 @@ func _input(event):
 		# keep rotation_degrees between 0 and 360
 		rotation_degrees = fposmod(rotation_degrees, 360.0)
 		$GunSprite.flip_v = rotation_degrees > 90 && rotation_degrees < 270
+		
+	if Input.is_action_pressed("shoot") and can_fire:
+		var bullet_instance = bullet.instance()
+		bullet_instance.rotation = rotation
+		bullet_instance.global_position = $Position2D.global_position
+		get_parent().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(0.2),"timeout")
+		can_fire = true
