2abddf6d4489f7e63c239f7c8b99ee7f381903c3
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2022-01-02 13:47:42 +0800

    chase and idle and animation add

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,6 +1,41 @@
 extends KinematicBody2D
 onready var stats = $Stats
-
+onready var pdz = $PlayerDetectionZone
+onready var slimeAni = $slimeAnimation
+export(int) var aclt = 300
+export(int) var max_speed = 50
+export(int) var frict = 200
+enum {
+	idle,
+	wander,
+	chase
+}
+var state = idle
+var velocity = Vector2.ZERO
+func _process(delta):
+	match state:
+		idle:
+			slimeAni.animation = "idle"
+			velocity = velocity.move_toward(Vector2.ZERO, frict*delta)
+			seek_player()
+		wander:
+			pass
+		chase:
+			var player = pdz.player
+			slimeAni.animation = "walk"
+			if pdz.can_see_player():
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction*max_speed, aclt*delta)
+			else:
+				state = idle
+			slimeAni.flip_h = velocity.x > 0
+	
+	velocity = move_and_slide(velocity)
+			
+func seek_player():
+	if pdz.can_see_player():
+		state = chase
+	
 func _on_hurtbox_area_entered(area):
 	stats.health -= area.damage
 
