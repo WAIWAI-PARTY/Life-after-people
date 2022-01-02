478b20005734180c7cf912d8ac8a7f89d546604d
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2022-01-02 16:56:28 +0800

    barrel EkusuPLOSIONNN!!!

--- World/Barrel.gd
+++ World/Barrel.gd
@@ -1,8 +1,11 @@
 extends KinematicBody2D
 var explosion = preload("res://Effect/Explosion_Effect.tscn")
-onready var hitbox = $hitbox/CollisionShape2D
+onready var hitbox = $hitbox
 func _on_hurtbox_area_entered(_area):
+	hitbox.scale = Vector2(1,1)
 	var edf = explosion.instance()
 	get_parent().add_child(edf)
 	edf.scale*=2.5
 	edf.global_position = global_position
+	yield(get_tree().create_timer(0.1), "timeout")
+	queue_free()
