86694e81a1b62bcbee4ea2d8b2de57b83d44d7c1
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2022-01-04 22:37:20 +0800

    dash fix

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,11 +1,13 @@
 extends KinematicBody2D
 
-export var maxSpeed = 250000
+export(int) var maxSpeed = 100
 
-const dash_speed = 800000
+export(int) var aclt = 800
+export(int) var frict = 1000
 const dash_duration = 0.2
 
 var stats = PlayerStats
+var velocity = Vector2.ZERO
 
 onready var sprite = $Sprite
 onready var hurtbox = $hurtbox
@@ -16,26 +18,23 @@ func _ready():
 
 	
 func _process(delta):
-	var move_direction = get_move_direction()
-	
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	maxSpeed = 450 if dash.is_dashing() else 100
+	aclt = 10000 if dash.is_dashing() else 800
 	if Input.is_action_just_pressed("dash") && dash.can_dash && !dash.is_dashing():
-		dash.start_dash(sprite, dash_duration, move_direction)
-			
-	var speed = dash_speed if dash.is_dashing() else maxSpeed
+		dash.start_dash(sprite, dash_duration, input_vector)
 	
-	
-
-	var velocity = move_direction.normalized() * speed * delta
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector*maxSpeed, aclt*delta)
+		
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, frict*delta)
+	print(velocity)
 	move_and_slide(velocity)
-	
 
-func get_move_direction():
-	return Vector2(
-		int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")),
-		int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
-	)
-	
-			
 func _on_hurtbox_area_entered(area):
 	if dash.is_dashing(): return
 	stats.health -= area.damage
