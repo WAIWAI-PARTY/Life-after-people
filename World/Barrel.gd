46d66421fb71677e261b64ace2302e9a5e98cd15
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2022-01-07 17:12:09 +0800

    explosion

--- World/Barrel.gd
+++ World/Barrel.gd
@@ -1,6 +1,10 @@
 extends KinematicBody2D
+
 onready var anim = $AnimationPlayer
+onready var sprite = $Sprite
+
 func _on_hurtbox_area_entered(_area):
+	sprite.scale = Vector2(4,4)
 	anim.play("anim")
 func explosion_finished():
 	queue_free()
