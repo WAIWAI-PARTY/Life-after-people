1d28fddd1927613ecdbec83fff1b4f226201b33f
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2022-01-08 17:22:30 +0800

    camera follow

--- Weapons/Gun.gd
+++ Weapons/Gun.gd
@@ -7,8 +7,7 @@ var bullet = preload("res://Weapons/Bullets.tscn")
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		look_at(event.position)
-
+		look_at(get_global_mouse_position())
 		# keep rotation_degrees between 0 and 360
 		rotation_degrees = fposmod(rotation_degrees, 360.0)
 		if rotation_degrees > 90 && rotation_degrees < 270:
