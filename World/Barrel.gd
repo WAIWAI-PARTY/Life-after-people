2c4e0d76553aa385b2f2c73e7dcf63644e6514e5
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2022-01-09 14:39:51 +0800

    camera shaking effect by  stealing

--- World/Barrel.gd
+++ World/Barrel.gd
@@ -1,10 +1,13 @@
 extends KinematicBody2D
 
+
 onready var anim = $AnimationPlayer
 onready var sprite = $Sprite
 
 func _on_hurtbox_area_entered(_area):
+	var shakeEffect = get_node("/root/World/Camera2D")
 	sprite.scale = Vector2(2.5,2.5)
 	anim.play("anim")
+	shakeEffect.shake(0.5, 30, 10)
 func explosion_finished():
 	queue_free()
