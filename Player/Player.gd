7457646f6f2c16874620b2f703411c5594727089
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2022-01-15 02:50:17 +0800

    add player idle animation

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,17 +5,22 @@ export(int) var aclt = 800
 export(int) var frict = 1000
 export(float) var dash_duration = 0.2
 export(float) var b_time_duration = 0.4
+
 var stats = PlayerStats
 var velocity = Vector2.ZERO
 var speed_factor = 1
 var input_vector = Vector2.ZERO
-enum {WALK, DASH, B_TIME}
-var state = WALK
+enum {IDLE, WALK, DASH, B_TIME}
+var state = IDLE
+
 onready var sprite = $Sprite
 onready var hurtbox = $hurtbox
 onready var dash = $Dash
 onready var bullet_time = $Bullet_Time
 onready var blink = $Blink
+onready var anim_tree = $AnimationTree
+onready var anim_player = $AnimationPlayer
+
 
 func _ready():
 	randomize()
@@ -28,8 +33,11 @@ func _process(delta):
 	maxSpeed = 450 if dash.is_dashing() else 100*speed_factor
 	aclt = 10000*speed_factor if dash.is_dashing() else 800*speed_factor
 	frict = 1000*speed_factor
+	anim_tree["parameters/idle/TimeScale/scale"] = speed_factor
 	$Sprite.flip_h = global_position>get_global_mouse_position()
 	match state:
+		IDLE:
+			idle()
 		WALK:
 			walk_state(delta)
 		DASH:
@@ -46,13 +54,21 @@ func walk_state(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, frict*delta)
 	if Input.is_action_just_pressed("dash") && dash.can_dash && !dash.is_dashing():
 		state = DASH
-	if Input.is_action_just_pressed("bullet_time") and bullet_time.can_bullet and !bullet_time.is_bulleting():
+	elif Input.is_action_just_pressed("bullet_time") and bullet_time.can_bullet and !bullet_time.is_bulleting():
 		state = B_TIME
+	elif velocity == Vector2.ZERO:
+		state = IDLE
 
 func dash_state():
 	dash.start_dash(sprite, dash_duration, input_vector)
 	state = WALK
-
+func idle():
+	if Input.is_action_just_pressed("dash") && dash.can_dash && !dash.is_dashing():
+		state = DASH
+	elif Input.is_action_just_pressed("bullet_time") and bullet_time.can_bullet and !bullet_time.is_bulleting():
+		state = B_TIME
+	elif input_vector != Vector2.ZERO:
+		state = WALK
 func b_time_state():
 	bullet_time.start_bullet_time(sprite, b_time_duration)
 	state = WALK
@@ -60,7 +76,7 @@ func b_time_state():
 func _on_hurtbox_area_entered(area):
 	if dash.is_dashing(): return
 	stats.health -= area.damage
-	hurtbox.start_invin(0.001)
+	hurtbox.start_invin(0.5)
 
 func _on_Dash_dash_end():
 	velocity = Vector2.ZERO
