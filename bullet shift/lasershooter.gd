1d42fcd0c00a20f28fff5c4e750aa4dfe532514e
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2022-09-18 18:23:00 +0800

    bullet refractor

--- bullet shift/lasershooter.gd
+++ bullet shift/lasershooter.gd
@@ -9,7 +9,7 @@ func _on_lasershooter_area_entered(area):
 		var laserIns = laser.instance()
 		laserIns.rotation_degrees = area.get_parent().rotation_degrees
 		laserIns.position = global_position
-		get_node("/root").add_child(laserIns)
+		get_node("/root").call_deferred("add_child", laserIns)
 		$Timer.start()
 
 func _on_Timer_timeout():
