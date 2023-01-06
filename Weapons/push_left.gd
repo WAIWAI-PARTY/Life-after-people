bbc853b41b0d173b9e867ece0ee65a9504ba8765
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2023-01-06 14:41:10 +0800

    psuh

--- Weapons/push_left.gd
+++ Weapons/push_left.gd
@@ -1,4 +1,4 @@
 extends Area2D
 
 func _on_left_area_entered(area):
-	area.get_parent().velocity = global_position.direction_to($"../left2".global_position)*50000*get_process_delta_time()
+	area.get_parent().velocity = global_position.direction_to($"../left2".global_position)*25000*get_process_delta_time()
