045ff92f583adeaaab7522fcbda56ede332241c5
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2023-01-06 17:37:28 +0800

    push

--- Weapons/dart.gd
+++ Weapons/dart.gd
@@ -6,11 +6,14 @@ var stats = PlayerStats
 var timerStop = true
 var can_fire = false
 var is_temp = false
+var angle  = 0
+var sp_timer = true
 
 export var weapon_name = "dart"
 export var noise = 0.001
 export var speed = 1200
 export var stay_time = 0.5
+export var special_stay_time = 5
 export var readyCD = 0.5
 onready var stay_timer = $stay_timer
 onready var tar_pos
@@ -21,7 +24,7 @@ var state = IDLE
 
 func _ready():
 	stats.playerStats["current_weapon"] = weapon_name
-	$readyTImer.start(readyCD)
+	$readyTimer.start(readyCD)
 
 func _process(delta):
 	if Input.is_action_pressed("shoot") and not(shooting) and can_fire:
@@ -36,7 +39,7 @@ func _process(delta):
 		FIRE:
 			firing(delta)
 		SPECIAL:
-			special()
+			special(delta)
 
 func idle():
 	stats.bullet_count(weapon_name, get_parent().name, 0)
@@ -59,16 +62,34 @@ func firing(delta):
 			coming_back = false
 			shooting = false
 			state = IDLE
-func special():
-	for i in 50:
-		var it_ins = itself.instance()
-		it_ins.state = FIRE
-		it_ins.is_temp = true
-		it_ins.stay_time = 3
-		it_ins.tar_pos = Vector2((i*5+100) * cos(rand_range(0,359)), (i*5+100) * sin(rand_range(0,359)))+global_position
-		get_parent().add_child(it_ins)
-	state = IDLE
-		
+
+func special(delta):
+	if sp_timer:
+		$special_stepper.start()
+		sp_timer = false
+	if angle >= 360:
+		angle = 0
+	var x = 50 * sin(PI * 2 * angle / 360);
+	var y = 50 * cos(PI * 2 * angle / 360);
+	print(str(x)+","+str(y))
+	tar_pos = Vector2(x,y)+get_parent().get_parent().global_position
+	$hitbox.damage = 20
+	if not(coming_back):
+		global_position = global_position.move_toward(tar_pos, speed*delta)
+		if timerStop:
+			stay_timer.start(special_stay_time)
+			timerStop = false
+	else:
+		global_position = global_position.move_toward(get_parent().global_position, speed*delta)
+		if global_position.distance_to(get_parent().global_position) < 0.1:
+			if is_temp:
+				queue_free()
+			position = Vector2.ZERO
+			coming_back = false
+			shooting = false
+			sp_timer = true
+			state = IDLE
+
 func _on_Timer_timeout():
 	coll.set_deferred("disabled", true)
 	coll.set_deferred("disabled", false)
@@ -76,6 +97,10 @@ func _on_Timer_timeout():
 func _on_stay_timer_timeout():
 	coming_back = true
 	timerStop = true
+	$special_stepper.stop()
 
-func _on_readyTImer_timeout():
+func _on_readyTimer_timeout():
 	can_fire = true
+
+func _on_special_stepper_timeout():
+	angle+=20
