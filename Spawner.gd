5d1a4e40bcf944e3b98d05bd3ad841b28d1cb606
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2023-01-06 12:45:21 +0800

    spawner limit

--- Spawner.gd
+++ Spawner.gd
@@ -3,6 +3,8 @@ extends Node2D
 var Enemy = preload("res://Enemy/Slime.tscn")
 
 func _on_SpawnTimer_timeout():
-	var enemy = Enemy.instance()
-	get_node("/root/World/YSort/slimes").add_child(enemy)
-	enemy.position = $Spawn.position
+	var	slimes = get_tree().get_nodes_in_group("slimeEnemy").size()
+	if slimes < 30:		
+		var enemy = Enemy.instance()
+		get_node("/root/World/YSort/slimes").add_child(enemy)
+		enemy.position = $Spawn.position
