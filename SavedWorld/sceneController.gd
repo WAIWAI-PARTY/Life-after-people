4119ee73875505ac367e63d9ee756c6b7a73b81b
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2023-01-23 23:13:48 +0800

    wai gor ho lun 7

--- SavedWorld/sceneController.gd
+++ SavedWorld/sceneController.gd
@@ -3,8 +3,7 @@ extends Node2D
 
 func _process(_delta):
 	if Input.is_action_just_pressed("time_travel"):
-		if get_tree().current_scene.name == "World":
-			get_tree().change_scene("res://SavedWorld/parallelWorld.tscn")
-		elif get_tree().current_scene.name == "parallelWorld":
-			get_tree().change_scene("res://SavedWorld/normalWorld.tscn")
-
+		$Camera2D.current = false
+		$parallelWorld/Camera2D.current = true
+		
+		
