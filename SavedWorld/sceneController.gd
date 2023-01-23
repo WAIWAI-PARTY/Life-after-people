2168d4366221e745f6e608aac25405c2921f877c
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2023-01-23 13:56:05 +0800

    generalized ranged weapon

--- SavedWorld/sceneController.gd
+++ SavedWorld/sceneController.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("time_travel"):
 		get_tree().change_scene("res://SavedWorld/parallelWorld.tscn")
 
--- ranged_weapon.gd
+++ ranged_weapon.gd
@@ -2,46 +2,59 @@ extends Node2D
 
 export(int) var max_ammo = 10
 export(int) var reserve_ammo = 30 setget set_reserve_ammo
-
+export(float) var max_recoil = 10.0
 export(float) var reload_time = 1.0
-var current_ammo = max_ammo
 export(float) var fire_rate = 0.13
+export(int) var bullet_type = 0
+export(int) var bullet_per_shot = 1
+export(int) var accuracy = 10
 
-onready var _shoot_position = get_parent().muzzle
+var current_recoil = 0.0
+var current_ammo = max_ammo
+var recoil_degree_actual = 0
 
 onready var _reload_timer := $ReloadTimer
 onready var _shoot_timer := $ShootTimer
-onready var camera = get_tree().get_nodes_in_group("camera")[0]
-onready var bullet_type = get_parent().bullet_type
-onready var bullet_per_shot = get_parent().bullet_per_shot
+
+onready var bullets = [preload("res://Weapons/Bullets.tscn"), preload("res://Weapons/push_off_bullet.tscn")]
+onready var BulletScene = bullets[bullet_type]
 
 func _ready() -> void:
 	_reload_timer.connect("timeout", self, "refill_ammo")
-	refill_ammo()
-
 
 func _process(_delta: float) -> void:
+	get_parent().look_at(get_global_mouse_position())
+	get_parent().rotation_degrees = fposmod(get_parent().rotation_degrees, 360.0)+recoil_degree_actual
+	if get_parent().rotation_degrees > 90 && get_parent().rotation_degrees < 270:
+		get_parent().set_scale(Vector2(1,-1))
+	else:
+		get_parent().set_scale(Vector2(1,1))
+		
 	if not _reload_timer.is_stopped():
 		return
-
 	if Input.is_action_pressed("shoot"):
 		if current_ammo > 0:
 			shoot()
 		else:
+			clamp_down_recoil()
 			reload()
 	elif Input.is_action_just_pressed("reload") and current_ammo < max_ammo:
 		reload()
-
+	else:
+		clamp_down_recoil()
 
 func shoot() -> void:
 	if not _shoot_timer.is_stopped():
 		return
 	current_ammo -= 1
-	for i in range(bullet_per_shot):
-		var bullet = get_parent().BulletScene.instance()
-		bullet.global_position = get_parent().muzzle.global_position
-		bullet.rotation = get_parent().rotation+rand_range(-get_parent().accuracy/100,get_parent().accuracy/100)
-		add_child(bullet)
+	recoil_degree_actual = rand_range(-current_recoil, current_recoil)
+	var recoil_increment = max_recoil*0.1
+	current_recoil = clamp(current_recoil+recoil_increment, 0.0, max_recoil)
+	for _i in range(bullet_per_shot):
+		var bullet = BulletScene.instance()
+		bullet.global_position = $muzzle.global_position
+		bullet.rotation = get_parent().rotation+rand_range(-accuracy/1000,accuracy/1000)
+		get_tree().get_root().add_child(bullet)
 
 	_shoot_timer.start(fire_rate)
 
@@ -64,6 +77,7 @@ func refill_ammo() -> void:
 func set_reserve_ammo(value: int) -> void:
 	reserve_ammo = value
 
-
-func _on_shaketimer_timeout():
-	camera.offset = lerp(camera.offset,Vector2.ZERO, 0.5)
+func clamp_down_recoil():
+	var recoil_increment = max_recoil*0.02
+	recoil_degree_actual = clamp(recoil_degree_actual-recoil_increment,0.0,max_recoil)
+	current_recoil = recoil_degree_actual
