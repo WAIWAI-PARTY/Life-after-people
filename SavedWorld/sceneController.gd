8982178f7fb8e5d908fc4f034781cf051f971da8
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2023-01-23 17:54:46 +0800

    construct parallel world i hate adjust pos

--- SavedWorld/sceneController.gd
+++ SavedWorld/sceneController.gd
@@ -3,5 +3,8 @@ extends Node2D
 
 func _process(_delta):
 	if Input.is_action_just_pressed("time_travel"):
-		get_tree().change_scene("res://SavedWorld/parallelWorld.tscn")
+		if get_tree().current_scene.name == "World":
+			get_tree().change_scene("res://SavedWorld/parallelWorld.tscn")
+		elif get_tree().current_scene.name == "parallelWorld":
+			get_tree().change_scene("res://SavedWorld/normalWorld.tscn")
 
