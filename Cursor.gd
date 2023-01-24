cdb49fcbfa9cde8109dd728c68519c918e6a28a4
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2023-01-24 12:55:33 +0800

    nicely done, 0 latency cursor

--- Cursor.gd
+++ Cursor.gd
@@ -1,7 +1,5 @@
 extends Node2D
 onready var aim_crosshair = $Crosshair
-func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 func _process(_delta):
 	aim_crosshair.global_position = get_global_mouse_position();
