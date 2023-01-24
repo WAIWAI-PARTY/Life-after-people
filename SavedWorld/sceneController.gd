84005a175696e43d828c8240860edde88a43883f
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2023-01-24 12:31:55 +0800

    "time travel" ez done in 3 min

--- SavedWorld/sceneController.gd
+++ SavedWorld/sceneController.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
-
+var travelled = false
 func _process(_delta):
 	if Input.is_action_just_pressed("time_travel"):
-		if get_tree().current_scene.name == "World":
-			get_tree().change_scene("res://SavedWorld/parallelWorld.tscn")
-		elif get_tree().current_scene.name == "parallelWorld":
-			get_tree().change_scene("res://SavedWorld/normalWorld.tscn")
+		if not travelled:
+			$YSort/Player.position.x+=2000
+			travelled = true
+		else:
+			$YSort/Player.position.x-=2000
+			travelled = false
+#		if get_tree().current_scene.name == "World":
+#			get_tree().change_scene("res://SavedWorld/parallelWorld.tscn")
+#		elif get_tree().current_scene.name == "parallelWorld":
+#			get_tree().change_scene("res://SavedWorld/normalWorld.tscn")
 
