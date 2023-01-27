a59685f58eb81e85d2c73b7562ce2999c97c6bbc
WAIWAI-PARTY <96366002+WAIWAI-PARTY@users.noreply.github.com>
2023-01-27 16:41:21 +0800

    good programming practice update

--- SavedWorld/sceneController.gd
+++ SavedWorld/sceneController.gd
@@ -4,10 +4,8 @@ var travelled = false
 func _process(_delta):
 	if Input.is_action_just_pressed("time_travel"):
 		if not travelled:
-			TimeTravelTransition.time_transition()
-			$YSort/Player.position.x+=2000
+			TimeTravelTransition.time_transition(2000)
 			travelled = true
 		else:
-			TimeTravelTransition.time_transition()
-			$YSort/Player.position.x-=2000
+			TimeTravelTransition.time_transition(-2000)
 			travelled = false
--- sceneTransition/time_travel_transition.gd
+++ sceneTransition/time_travel_transition.gd
@@ -1,10 +1,12 @@
 extends CanvasLayer
 
-signal transite 
-func time_transition() -> void:
-		$AnimationPlayer.play("dissolve")
-		yield($AnimationPlayer, "animation_finished")
-		$AnimationPlayer.play_backwards("dissolve")
+signal transite
+var travel_distance = 0
+func time_transition(value) -> void:
+	travel_distance = value
+	$AnimationPlayer.play("dissolve")
 
-
-
+func _on_AnimationPlayer_animation_finished(anim_name):
+	if anim_name == "dissolve":
+		get_tree().get_nodes_in_group("player")[0].position.x += travel_distance
+		$AnimationPlayer.play("dissolve_back")
