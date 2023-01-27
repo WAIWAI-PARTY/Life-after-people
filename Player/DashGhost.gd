a3ef567c73704f246a95a494e85774bb8fc01173
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2023-01-27 15:58:23 +0800

    Revert "rgb on"
    This reverts commit 4e5270e46bfbb53f25ced1ecfbdbc02d7536cc9a.

--- Player/DashGhost.gd
+++ Player/DashGhost.gd
@@ -1,11 +1,9 @@
 extends Sprite
-var gay_mode = false
+
 func _ready():
-	set("modulate:s", 50)
 	$Tween.interpolate_property(self, "modulate:a", 1.0, 0.0 , 2, 3 ,1)
-	if gay_mode:
-		$Tween.interpolate_property(self, "modulate:s", 1.0, 0.0 , 2, 3 ,1)
-		$Tween.interpolate_property(self, "modulate:h", 1.0, 0.0 , 2, 3 ,1)
 	$Tween.start()
+
+
 func _on_Tween_tween_completed(_object, _key):
 	queue_free()
