51d603938cca5c762adae3a03c170dba87c683ca
Civil1394 <lamkaito135@gmail.com>
2023-02-09 18:24:12 +0800

    fk the cam

--- SavedWorld/sceneController.gd
+++ SavedWorld/sceneController.gd
@@ -1,11 +1,16 @@
 extends Node2D
 
 var travelled = false
+var currentCamera
 func _process(_delta):
 	if Input.is_action_just_pressed("time_travel"):
 		if not travelled:
-			TimeTravelTransition.time_transition(2000)
 			travelled = true
+			$CanvasLayer/Label.text ="i am in past"
+			TimeTravelTransition.time_transition(10000)
 		else:
-			TimeTravelTransition.time_transition(-2000)
 			travelled = false
+			$CanvasLayer/Label.text ="i am in future"
+			TimeTravelTransition.time_transition(-10000)
+
+
