faad2b53ca5d7446c2052e24883e5f5abe502b5e
Civil1394 <lamkaito135@gmail.com>
2023-02-11 16:30:10 +0800

    wait for wai god group

--- SavedWorld/sceneController.gd
+++ SavedWorld/sceneController.gd
@@ -1,14 +1,16 @@
 extends Node2D
 
+
 var travelled = false
 var currentCamera
 func _process(_delta):
 	if Input.is_action_just_pressed("time_travel"):
 		if not travelled:
 			travelled = true
+			#movePlayerParent(travelled)
 			#$Camera2DFuture.limit_left = -100000
 			$CanvasLayer/Label.text ="i am in past"
-			TimeTravelTransition.time_transition(10000)
+			#TimeTravelTransition.time_transition(10000)
 			#$Camera2DFuture.limit_left = -140+10000
 		else:
 			travelled = false
@@ -17,3 +19,9 @@ func _process(_delta):
 			TimeTravelTransition.time_transition(-10000)
 			#$Camera2DFuture.limit_left = -140
 
+func movePlayerParent(travelled):
+	if travelled == true:
+		var player_node = get_tree().get_nodes_in_group("player")
+		player_node.get_parent().remove_child(player_node)
+		get_node("PastWorld/Pastprop").add_child(player_node)
+	
