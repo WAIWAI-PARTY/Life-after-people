46ab59dca8cfc556ec288ca601afa415fad0c101
Civil1394 <96367948+Civil1394@users.noreply.github.com>
2023-02-11 12:25:57 +0800

    fixed tilemap fix prop ysort then

--- SavedWorld/DemoScenes/stage1.gd
+++ SavedWorld/DemoScenes/stage1.gd
@@ -7,7 +7,8 @@ extends Area2D
 
 func _on_stage1_body_entered(body):
 	$"../../Camera2DFuture".limit_left = -60
-
+	$"../Futureprop/Player".z_index = 3
 
 func _on_stage1_body_exited(body):
 	$"../../Camera2DFuture".limit_left = -250
+	$"../Futureprop/Player".z_index = 0
